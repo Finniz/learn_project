5f1bb0f8dd951cf5830728e09eea9a317757d6b7
Mark Dumler <markoman3112@gmail.com>
2021-10-15 00:32:59 +0300

    Добавлен личный кабинет

---
+++ webapp/user_page/views.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+blueprint = Blueprint('user_page', __name__, url_prefix='/user')
+
+@blueprint.route("/")
+def user():
+    title = "Личный кабинет"
+    return render_template('user_page/user.html', page_title=title)
