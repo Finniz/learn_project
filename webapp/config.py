161dc6aad365ccd6a1e3c11d4d068f0cdd2a0f6e
Mark Dumler <markoman3112@gmail.com>
2021-10-16 10:26:42 +0300

    added pages

---
+++ webapp/config.py
@@ -0,0 +1,10 @@
+from datetime import timedelta
+
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+SECRET_KEY = 'dsD243k%2lkjskad6&!@dfkslodjak'
+REMEMBER_COOKIE_DURATION = timedelta(days=14)
