a3f9cee4cef6f428191c3fe149ebf41cbaefcaa2
Yury Puchenkin <ypuchenkin@gmail.com>
2021-10-27 20:44:52 +0300

    SQLAlchemy > Flask_sqlalchemy

--- webapp/dbase.py
+++ webapp/dbase.py
@@ -7,7 +7,7 @@ engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
-Base.query = db.session.query_property()
+Base.query = db_session.query_property()
 
 
 def init_db():
